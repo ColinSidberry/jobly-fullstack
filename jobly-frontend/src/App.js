a6a0115b3b9b4a45a249cfca5946d0dd96368ce2
Colin Sidberry <colinsidberry@gmail.com>
2021-10-21 16:02:01 -0700

    Updated api.js to include user routes and update App.js with useEffect and UserContext and login,signup, and logoutuser function.

--- jobly-frontend/src/App.js
+++ jobly-frontend/src/App.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import LoggedInNav from "./LoggedInNav";
 import LoggedOutNav from "./LoggedOutNav";
 import Routes from "./Routes";
 import { BrowserRouter } from 'react-router-dom';
+import JoblyApi from "./api";
+import UserContext from "./UserContext";
 
 
 /** Renders Routes and Nav Components. 
@@ -18,32 +20,79 @@ import { BrowserRouter } from 'react-router-dom';
 function App() {
   const [currUser, setCurrUser] = useState(null);
   const [token, setToken] = useState(null);
+  // const initialErrors = {
+  //   profileForm: null, 
+  //   loginIn: null, 
+  //   signupUser: null, 
+  //   userData:null
+  // }
+  //Question: Do we have to specify errors like above?
+
+  const [errors, setErrors] = useState(null)
+  //TODO: How do we pass errors down to each of the forms
+
+  //effect: set currUser when token changes
+  useEffect(function fetchUserDataOnTokenChange() {
+    async function fetchUserData() {
+      try {
+        const userData = await JoblyApi.getUser(token);
+        setCurrUser(userData);
+      } catch (err) {
+        setErrors(err);
+
+      }
+    }
+    fetchUserData();
+  }, [token]);
 
   async function loginUser(formData) {
-    
+    try {
+      const token = await JoblyApi.login(formData);
+      setToken(token);
+    } catch (err) {
+      setErrors(err);
+    }
   }
 
   async function signupUser(formData) {
-    
+    try {
+      const token = await JoblyApi.register(formData);
+      setToken(token);
+    } catch (err) {
+      setErrors(err);
+    }
   }
 
-  async function logoutUser() {
-    setCurrUser(null);
-    setToken(null); //???? 
+  function logoutUser() {
+    try {
+      const token = JoblyApi.logout();
+      setToken(token);
+    } catch (err) {
+      setErrors(err);
+    }
   }
 
   //udapteUserInfo Function 
   //validates data, if errors->set errors , and errors are passed down
-  // App: errors = {profileErros: error}
   //decide user context, currUser is state
+  function updateUserInfo()
+
   return (
     <div>
-      <BrowserRouter>
-        {currUser
-          ? <LoggedInNav />
-          : <LoggedOutNav />}
-        <Routes />
-      </BrowserRouter>
+      <UserContext.Provider value={currUser}>
+        <BrowserRouter>
+          {currUser
+            ? <LoggedInNav 
+                updateUserInfo={updateUserInfo} 
+                logoutUser={logoutUser}
+                errors={errors}/> 
+            : <LoggedOutNav
+                signupUser={signupUser}
+                loginUser={loginUser} 
+                errors={errors}/>}
+          <Routes />
+        </BrowserRouter>
+      </UserContext.Provider>
     </div>
   );
 }
--- jobly-frontend/src/api.js
+++ jobly-frontend/src/api.js
@@ -1,4 +1,7 @@
 import axios from "axios";
+const jwt = require("jsonwebtoken");
+//Question: is the config folder a shared resource in that we should verify the token
+// or should we just decode the token?
 
 const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
@@ -38,6 +41,8 @@ class JoblyApi {
 
   // Individual API routes
 
+  ///COMPANY FUNCTIONS-----------------------------------
+
   /** Get details on a company by handle. 
    * input: handle - company handle as a string
    * output: { handle, name, description, numEmployees, logoUrl, jobs }
@@ -75,11 +80,11 @@ class JoblyApi {
     return res.jobs;
   }
 
-  ///AUTH FUNCTIONS
+  ///AUTH FUNCTIONS-----------------------------------
 
   /**Login user with {username, passsword}. Returns token. */
   static async login({ username, password }) {
-    let res = await this.request(`token`, { username, password });
+    let res = await this.request(`/auth/token`, { username, password }, "post");
     this.token = res.token;
     console.log('token is: ', res.token);
     return res.token;
@@ -87,7 +92,10 @@ class JoblyApi {
   /**Register user with { username, password, firstName, lastName, email }.
    * Returns token. */
   static async register({ username, password, firstName, lastName, email }) {
-    let res = await this.request(`register`, { username, password, firstName, lastName, email });
+    let res = await this.request(
+      `/auth/register`,
+      { username, password, firstName, lastName, email },
+      "post");
     this.token = res.token;
     console.log('token is: ', res.token);
     return res.token;
@@ -99,6 +107,29 @@ class JoblyApi {
     return this.token;
   }
 
+  ///USER FUNCTIONS-----------------------------------
+  /** Gets user information
+    * input: token
+    * output: userData - { username, firstName, lastName, isAdmin, jobs }
+    *     where jobs is { id, title, companyHandle, companyName, state }
+  */
+  static async getUser(token) {
+    const username = jwt.decode(token).username;
+    let res = await this.request(`/users/${username}`);
+    return res.user;
+  }
+
+  /** Updates user info if pw is valid
+    * input: formData -  { username, firstName, lastName, password, email }
+    * output: userData - { username, firstName, lastName, isAdmin }
+  */
+  static async updateUser(formData) {
+    const { username } = formData;
+    delete formData[username];
+
+    let res = await this.request(`/users/${username}`, formData, "patch");
+    return res.user;
+  }
 }
 
 export default JoblyApi;
